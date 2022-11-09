91a4f264bc7ddb0ea5a5ea41dc06fe6896cfbdd3
SamPoly21 <muminsamiul@gmail.com>
2022-11-08 22:37:39 -0500

    9th

---
+++ js/script.js
@@ -0,0 +1,63 @@
+// Determine if a field is blank
+function isBlank(inputField){
+    if (inputField.value=="") {
+        return true;
+    }
+    return false;
+}
+
+
+// remove all error styles from the div passed in
+function makeClean(element){
+    element.classList.remove("error");		
+}
+
+function displayText(){
+    {document.getElementById("errors").innerHTML = document.getElementById("errors").value;}
+}
+
+
+window.addEventListener("load", function() {    
+
+    var hilightableInputs = document.querySelectorAll(".hilightable");
+    console.log(hilightableInputs);
+    for (var i=0; i < hilightableInputs.length; i++) {
+        hilightableInputs[i].addEventListener("focus", function(e) 
+    {
+            e.target.classList.toggle(".highlight");    
+        });
+        hilightableInputs[i].addEventListener("blur", function(e) 
+    {
+            e.target.classList.toggle(".highlight");
+        });
+    }
+
+    var requiredInputs = document.querySelectorAll(".required");
+    console.log(requiredInputs );
+        for (var i=0; i < requiredInputs.length;i++) {
+        requiredInputs[i].addEventListener("change", function(e) {
+            makeClean(e.target);
+        });
+    }
+
+    var mainForm = document.getElementById("mainForm");
+    mainForm.addEventListener("submit", function(e) {
+    var requiredInputs = document.querySelectorAll(".required");
+        for (var i=0; i < requiredInputs.length; i++){
+            if( isBlank(requiredInputs[i]) ){
+                e.preventDefault();
+                requiredInputs[i].classList.add("error");;
+            }
+            else {
+                makeClean(requiredInputs[i]);
+            }
+        }
+        
+
+    });
+
+
+
+});
+
+
